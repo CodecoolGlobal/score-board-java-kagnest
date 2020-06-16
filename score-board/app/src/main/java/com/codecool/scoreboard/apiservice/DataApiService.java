97a535c2202d9280451a62552db7d2bf379d3f92
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-06-16 12:05:35 +0200

    Add model class

--- score-board/app/src/main/java/com/codecool/scoreboard/apiservice/DataApiService.java
+++ score-board/app/src/main/java/com/codecool/scoreboard/apiservice/DataApiService.java
@@ -1,4 +1,14 @@
 package com.codecool.scoreboard.apiservice;
 
+import com.codecool.scoreboard.model.TeamResponse;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface DataApiService {
+
+    @GET("lookupteam.php")
+    Single<TeamResponse> getTeamById(@Query("id") int teamId);
+
 }
