ffe2507d8755462efe1561e33c8e8b21c719ac68
Enci21 <eni.hegedus.95@gmail.com>
2020-06-16 12:40:07 +0200

    Implement the necessary methods

--- score-board/app/src/main/java/com/codecool/scoreboard/LastEventFragmentCollectionAdapter.java
+++ score-board/app/src/main/java/com/codecool/scoreboard/LastEventFragmentCollectionAdapter.java
@@ -1,4 +1,31 @@
 package com.codecool.scoreboard;
 
-public class LastEventFragmentCollectionAdapter {
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class LastEventFragmentCollectionAdapter extends FragmentStatePagerAdapter {
+
+    public LastEventFragmentCollectionAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        LastEventsFragment lastEventsFragment = new LastEventsFragment();
+        Bundle bundle = new Bundle();
+        position = position + 1;
+        lastEventsFragment.setArguments(bundle);
+        return lastEventsFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
 }
