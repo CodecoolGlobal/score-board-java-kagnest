710f248dfa2a1cea9ba77f17e458ec4bd839522f
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-06-16 14:58:52 +0200

    Define TeamResponse model class

--- score-board/app/src/main/java/com/codecool/scoreboard/model/DataAdapter.java
+++ score-board/app/src/main/java/com/codecool/scoreboard/model/DataAdapter.java
@@ -1,4 +1,41 @@
 package com.codecool.scoreboard.model;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DataAdapter {
+
+    public Team convertToTeam(TeamResponse.Teams response){
+        String leagues = createLeaguesText(response);
+        return new Team(response.getStrTeam(),
+                response.getIdTeam(),
+                response.getStrAlternate(),
+                response.getIntFormedYear(),
+                response.getStrSport(),
+                leagues,
+                response.getStrStadium(),
+                response.getStrStadiumThumb(),
+                response.getStrStadiumDescription(),
+                response.getStrWebsite(),
+                response.getStrDescriptionEN(),
+                response.getStrTeamLogo(),
+                response.getStrYoutube()
+                );
+    }
+
+    private String createLeaguesText(TeamResponse.Teams response){
+        List<String> leaguesList = new ArrayList<>(Arrays.asList(response.getStrLeague(), response.getIdLeague2(),
+                response.getIdLeague3(), response.getIdLeague4(), response.getIdLeague5()));
+        leaguesList.removeAll(Arrays.asList("", null));
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < leaguesList.size() - 1) {
+            sb.append(leaguesList.get(i));
+            sb.append("\n\n");
+            i++;
+        }
+        sb.append(leaguesList.get(i));
+        return sb.toString();
+    }
 }
--- score-board/app/src/main/java/com/codecool/scoreboard/model/TeamResponse.java
+++ score-board/app/src/main/java/com/codecool/scoreboard/model/TeamResponse.java
@@ -1,5 +1,489 @@
 package com.codecool.scoreboard.model;
 
+import java.util.List;
+
 public class TeamResponse {
+
+    private List<Teams> teams;
+
+    public List<Teams> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Teams> teams) {
+        this.teams = teams;
+    }
+
+    public class Teams {
+        private String strLocked;
+        private String strYoutube;
+        private String strTeamBanner;
+        private String strTeamFanart4;
+        private String strTeamFanart3;
+        private String strTeamFanart2;
+        private String strTeamFanart1;
+        private String strTeamLogo;
+        private String strTeamJersey;
+        private String strTeamBadge;
+        private String strCountry;
+        private String strGender;
+        private String strDescriptionNO;
+        private String strDescriptionPT;
+        private String strDescriptionES;
+        private String strDescriptionRU;
+        private String strDescriptionJP;
+        private String strDescriptionIT;
+        private String strDescriptionFR;
+        private String strDescriptionDE;
+        private String strDescriptionEN;
+        private String strInstagram;
+        private String strTwitter;
+        private String strFacebook;
+        private String strWebsite;
+        private String intStadiumCapacity;
+        private String strStadiumLocation;
+        private String strStadiumDescription;
+        private String strStadiumThumb;
+        private String strRSS;
+        private String strKeywords;
+        private String strStadium;
+        private String strManager;
+        private String idLeague5;
+        private String strLeague5;
+        private String idLeague4;
+        private String strLeague4;
+        private String idLeague3;
+        private String strLeague3;
+        private String idLeague2;
+        private String strLeague2;
+        private String idLeague;
+        private String strLeague;
+        private String strSport;
+        private String intFormedYear;
+        private String strAlternate;
+        private String strTeamShort;
+        private String strTeam;
+        private String intLoved;
+        private String idAPIfootball;
+        private String idSoccerXML;
+        private String idTeam;
+
+
+        public String getStrLocked() {
+            return strLocked;
+        }
+
+        public void setStrLocked(String strLocked) {
+            this.strLocked = strLocked;
+        }
+
+        public String getStrYoutube() {
+            return strYoutube;
+        }
+
+        public void setStrYoutube(String strYoutube) {
+            this.strYoutube = strYoutube;
+        }
+
+        public String getStrTeamBanner() {
+            return strTeamBanner;
+        }
+
+        public void setStrTeamBanner(String strTeamBanner) {
+            this.strTeamBanner = strTeamBanner;
+        }
+
+        public String getStrTeamFanart4() {
+            return strTeamFanart4;
+        }
+
+        public void setStrTeamFanart4(String strTeamFanart4) {
+            this.strTeamFanart4 = strTeamFanart4;
+        }
+
+        public String getStrTeamFanart3() {
+            return strTeamFanart3;
+        }
+
+        public void setStrTeamFanart3(String strTeamFanart3) {
+            this.strTeamFanart3 = strTeamFanart3;
+        }
+
+        public String getStrTeamFanart2() {
+            return strTeamFanart2;
+        }
+
+        public void setStrTeamFanart2(String strTeamFanart2) {
+            this.strTeamFanart2 = strTeamFanart2;
+        }
+
+        public String getStrTeamFanart1() {
+            return strTeamFanart1;
+        }
+
+        public void setStrTeamFanart1(String strTeamFanart1) {
+            this.strTeamFanart1 = strTeamFanart1;
+        }
+
+        public String getStrTeamLogo() {
+            return strTeamLogo;
+        }
+
+        public void setStrTeamLogo(String strTeamLogo) {
+            this.strTeamLogo = strTeamLogo;
+        }
+
+        public String getStrTeamJersey() {
+            return strTeamJersey;
+        }
+
+        public void setStrTeamJersey(String strTeamJersey) {
+            this.strTeamJersey = strTeamJersey;
+        }
+
+        public String getStrTeamBadge() {
+            return strTeamBadge;
+        }
+
+        public void setStrTeamBadge(String strTeamBadge) {
+            this.strTeamBadge = strTeamBadge;
+        }
+
+        public String getStrCountry() {
+            return strCountry;
+        }
+
+        public void setStrCountry(String strCountry) {
+            this.strCountry = strCountry;
+        }
+
+        public String getStrGender() {
+            return strGender;
+        }
+
+        public void setStrGender(String strGender) {
+            this.strGender = strGender;
+        }
+
+        public String getStrDescriptionNO() {
+            return strDescriptionNO;
+        }
+
+        public void setStrDescriptionNO(String strDescriptionNO) {
+            this.strDescriptionNO = strDescriptionNO;
+        }
+
+        public String getStrDescriptionPT() {
+            return strDescriptionPT;
+        }
+
+        public void setStrDescriptionPT(String strDescriptionPT) {
+            this.strDescriptionPT = strDescriptionPT;
+        }
+
+        public String getStrDescriptionES() {
+            return strDescriptionES;
+        }
+
+        public void setStrDescriptionES(String strDescriptionES) {
+            this.strDescriptionES = strDescriptionES;
+        }
+
+        public String getStrDescriptionRU() {
+            return strDescriptionRU;
+        }
+
+        public void setStrDescriptionRU(String strDescriptionRU) {
+            this.strDescriptionRU = strDescriptionRU;
+        }
+
+        public String getStrDescriptionJP() {
+            return strDescriptionJP;
+        }
+
+        public void setStrDescriptionJP(String strDescriptionJP) {
+            this.strDescriptionJP = strDescriptionJP;
+        }
+
+        public String getStrDescriptionIT() {
+            return strDescriptionIT;
+        }
+
+        public void setStrDescriptionIT(String strDescriptionIT) {
+            this.strDescriptionIT = strDescriptionIT;
+        }
+
+        public String getStrDescriptionFR() {
+            return strDescriptionFR;
+        }
+
+        public void setStrDescriptionFR(String strDescriptionFR) {
+            this.strDescriptionFR = strDescriptionFR;
+        }
+
+        public String getStrDescriptionDE() {
+            return strDescriptionDE;
+        }
+
+        public void setStrDescriptionDE(String strDescriptionDE) {
+            this.strDescriptionDE = strDescriptionDE;
+        }
+
+        public String getStrDescriptionEN() {
+            return strDescriptionEN;
+        }
+
+        public void setStrDescriptionEN(String strDescriptionEN) {
+            this.strDescriptionEN = strDescriptionEN;
+        }
+
+        public String getStrInstagram() {
+            return strInstagram;
+        }
+
+        public void setStrInstagram(String strInstagram) {
+            this.strInstagram = strInstagram;
+        }
+
+        public String getStrTwitter() {
+            return strTwitter;
+        }
+
+        public void setStrTwitter(String strTwitter) {
+            this.strTwitter = strTwitter;
+        }
+
+        public String getStrFacebook() {
+            return strFacebook;
+        }
+
+        public void setStrFacebook(String strFacebook) {
+            this.strFacebook = strFacebook;
+        }
+
+        public String getStrWebsite() {
+            return strWebsite;
+        }
+
+        public void setStrWebsite(String strWebsite) {
+            this.strWebsite = strWebsite;
+        }
+
+        public String getIntStadiumCapacity() {
+            return intStadiumCapacity;
+        }
+
+        public void setIntStadiumCapacity(String intStadiumCapacity) {
+            this.intStadiumCapacity = intStadiumCapacity;
+        }
+
+        public String getStrStadiumLocation() {
+            return strStadiumLocation;
+        }
+
+        public void setStrStadiumLocation(String strStadiumLocation) {
+            this.strStadiumLocation = strStadiumLocation;
+        }
+
+        public String getStrStadiumDescription() {
+            return strStadiumDescription;
+        }
+
+        public void setStrStadiumDescription(String strStadiumDescription) {
+            this.strStadiumDescription = strStadiumDescription;
+        }
+
+        public String getStrStadiumThumb() {
+            return strStadiumThumb;
+        }
+
+        public void setStrStadiumThumb(String strStadiumThumb) {
+            this.strStadiumThumb = strStadiumThumb;
+        }
+
+        public String getStrRSS() {
+            return strRSS;
+        }
+
+        public void setStrRSS(String strRSS) {
+            this.strRSS = strRSS;
+        }
+
+        public String getStrKeywords() {
+            return strKeywords;
+        }
+
+        public void setStrKeywords(String strKeywords) {
+            this.strKeywords = strKeywords;
+        }
+
+        public String getStrStadium() {
+            return strStadium;
+        }
+
+        public void setStrStadium(String strStadium) {
+            this.strStadium = strStadium;
+        }
+
+        public String getStrManager() {
+            return strManager;
+        }
+
+        public void setStrManager(String strManager) {
+            this.strManager = strManager;
+        }
+
+        public String getIdLeague5() {
+            return idLeague5;
+        }
+
+        public void setIdLeague5(String idLeague5) {
+            this.idLeague5 = idLeague5;
+        }
+
+        public String getStrLeague5() {
+            return strLeague5;
+        }
+
+        public void setStrLeague5(String strLeague5) {
+            this.strLeague5 = strLeague5;
+        }
+
+        public String getIdLeague4() {
+            return idLeague4;
+        }
+
+        public void setIdLeague4(String idLeague4) {
+            this.idLeague4 = idLeague4;
+        }
+
+        public String getStrLeague4() {
+            return strLeague4;
+        }
+
+        public void setStrLeague4(String strLeague4) {
+            this.strLeague4 = strLeague4;
+        }
+
+        public String getIdLeague3() {
+            return idLeague3;
+        }
+
+        public void setIdLeague3(String idLeague3) {
+            this.idLeague3 = idLeague3;
+        }
+
+        public String getStrLeague3() {
+            return strLeague3;
+        }
+
+        public void setStrLeague3(String strLeague3) {
+            this.strLeague3 = strLeague3;
+        }
+
+        public String getIdLeague2() {
+            return idLeague2;
+        }
+
+        public void setIdLeague2(String idLeague2) {
+            this.idLeague2 = idLeague2;
+        }
+
+        public String getStrLeague2() {
+            return strLeague2;
+        }
+
+        public void setStrLeague2(String strLeague2) {
+            this.strLeague2 = strLeague2;
+        }
+
+        public String getIdLeague() {
+            return idLeague;
+        }
+
+        public void setIdLeague(String idLeague) {
+            this.idLeague = idLeague;
+        }
+
+        public String getStrLeague() {
+            return strLeague;
+        }
+
+        public void setStrLeague(String strLeague) {
+            this.strLeague = strLeague;
+        }
+
+        public String getStrSport() {
+            return strSport;
+        }
+
+        public void setStrSport(String strSport) {
+            this.strSport = strSport;
+        }
+
+        public String getIntFormedYear() {
+            return intFormedYear;
+        }
+
+        public void setIntFormedYear(String intFormedYear) {
+            this.intFormedYear = intFormedYear;
+        }
+
+        public String getStrAlternate() {
+            return strAlternate;
+        }
+
+        public void setStrAlternate(String strAlternate) {
+            this.strAlternate = strAlternate;
+        }
+
+        public String getStrTeamShort() {
+            return strTeamShort;
+        }
+
+        public void setStrTeamShort(String strTeamShort) {
+            this.strTeamShort = strTeamShort;
+        }
+
+        public String getStrTeam() {
+            return strTeam;
+        }
+
+        public void setStrTeam(String strTeam) {
+            this.strTeam = strTeam;
+        }
+
+        public String getIntLoved() {
+            return intLoved;
+        }
+
+        public void setIntLoved(String intLoved) {
+            this.intLoved = intLoved;
+        }
+
+        public String getIdAPIfootball() {
+            return idAPIfootball;
+        }
+
+        public void setIdAPIfootball(String idAPIfootball) {
+            this.idAPIfootball = idAPIfootball;
+        }
+
+        public String getIdSoccerXML() {
+            return idSoccerXML;
+        }
+
+        public void setIdSoccerXML(String idSoccerXML) {
+            this.idSoccerXML = idSoccerXML;
+        }
+
+        public String getIdTeam() {
+            return idTeam;
+        }
+
+        public void setIdTeam(String idTeam) {
+            this.idTeam = idTeam;
+        }
+    }
 
 }
