72b9971b67287e3a8af74d86562296505ebce696
Enci21 <eni.hegedus.95@gmail.com>
2020-06-16 12:19:23 +0200

    Add new LastEventsFragment

---
+++ score-board/app/src/main/java/com/codecool/scoreboard/LastEventsFragment.java
@@ -0,0 +1,24 @@
+package com.codecool.scoreboard;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LastEventsFragment extends Fragment {
+
+
+
+    public LastEventsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_last_events, container, false);
+    }
+}
