90ce1dfd15ef1d71835c8d58a02fec1c378bfda5
Enci21 <eni.hegedus.95@gmail.com>
2020-06-17 14:43:02 +0200

    Refactor

--- score-board/app/src/main/java/com/codecool/scoreboard/MatchDetailsActivity.java
+++ score-board/app/src/main/java/com/codecool/scoreboard/MatchDetailsActivity.java
@@ -5,18 +5,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.codecool.scoreboard.apiservice.DataApiService;
-import com.codecool.scoreboard.apiservice.RetrofitClient;
 import com.codecool.scoreboard.model.Match;
-import com.codecool.scoreboard.model.MatchResponse;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Retrofit;
 
 public class MatchDetailsActivity extends AppCompatActivity {
 
@@ -54,9 +46,7 @@ public class MatchDetailsActivity extends AppCompatActivity {
     TextView awayYellowCard;
 
     String matchId;
-    DataApiService apiService;
-    Retrofit retrofit;
-    CompositeDisposable compositeDisposable = new CompositeDisposable();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,38 +57,7 @@ public class MatchDetailsActivity extends AppCompatActivity {
         //Intent intent = getIntent();
         //matchId = intent.getStringExtra();
 
-        retrofit = RetrofitClient.getClient();
-        apiService = retrofit.create(DataApiService.class);
-        fetchData();
     }
-
-    private void fetchData() {
-        compositeDisposable.add(apiService.getMatchById(matchId)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MatchResponse>() {
-                    @Override
-                    public void accept(MatchResponse matchResponse) throws Exception {
-                        Match match = new Match(
-                                matchResponse.getIdEvent(),
-                                matchResponse.getStrEvent(),
-                                matchResponse.getStrLeague(),
-                                matchResponse.getStrSeason(),
-                                matchResponse.getDateEvent(),
-                                matchResponse.getStrTime(),
-                                Integer.parseInt(matchResponse.getIntHomeScore()),
-                                Integer.parseInt(matchResponse.getIntAwayScore()),
-                                matchResponse.getStrHomeGoalDetails(),
-                                matchResponse.getStrHomeYellowCards(),
-                                matchResponse.getStrAwayGoalDetails(),
-                                matchResponse.getStrAwayYellowCards()
-                        );
-
-                        displayData(match);
-                    }
-                }));
-    }
-
     private void displayData(Match match) {
         event.setText(match.getEvent());
         league.setText(match.getLeague());
@@ -112,6 +71,4 @@ public class MatchDetailsActivity extends AppCompatActivity {
         awayGoalDetails.setText(match.getAwayGoalDetails());
         awayYellowCard.setText(match.getAwayYellowCards());
     }
-
-
 }
