ba65a8c9976227a41622a600b88ebdb038cedb8c
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-06-17 11:41:50 +0200

    Create team contract

---
+++ score-board/app/src/main/java/com/codecool/scoreboard/teamdetails/TeamContract.java
@@ -0,0 +1,19 @@
+package com.codecool.scoreboard.teamdetails;
+
+import com.codecool.scoreboard.BasePresenter;
+import com.codecool.scoreboard.BaseView;
+import com.codecool.scoreboard.model.Team;
+
+public interface TeamContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void showTeam(Team team);
+
+        void showErrorMessage(String errorMessage);
+    }
+
+    interface Presenter extends BasePresenter {
+        void getTeamById(int id);
+    }
+}
