e55a07a33be4ca245c24b77d3044bb4445c3d465
Enci21 <eni.hegedus.95@gmail.com>
2020-06-17 14:40:34 +0200

    Add recyclerview to EventsFragment

--- score-board/app/src/main/java/com/codecool/scoreboard/EventsFragment.java
+++ score-board/app/src/main/java/com/codecool/scoreboard/EventsFragment.java
@@ -6,9 +6,31 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codecool.scoreboard.apiservice.DataApiService;
+import com.codecool.scoreboard.apiservice.RetrofitClient;
+import com.codecool.scoreboard.model.MatchResponse;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
 
 public class EventsFragment extends Fragment {
 
+    DataApiService apiService;
+    Retrofit retrofit;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    EventListAdapter adapter;
+
+    @BindView(R.id.recyclerEvents)
+    RecyclerView recyclerView;
 
     public EventsFragment() {
         // Required empty public constructor
@@ -19,6 +41,26 @@ public class EventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View root = inflater.inflate(R.layout.fragment_events, container, false);
+        ButterKnife.bind(this, root);
+        retrofit = RetrofitClient.getClient();
+        apiService = retrofit.create(DataApiService.class);
+
+        fetchData();
+        return root;
+    }
+
+    private void fetchData() {
+        compositeDisposable.add(apiService.getListOfMatchById()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<MatchResponse>>() {
+                               @Override
+                               public void accept(List<MatchResponse> matchResponses) throws Exception {
+                                   adapter = new EventListAdapter(matchResponses);
+                                   recyclerView.setAdapter(adapter);
+                               }
+                           }
+                ));
     }
 }
