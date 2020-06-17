2ab607dc14ffa3326cad3e7405dff5bdb936ce2b
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-06-17 11:42:44 +0200

    Create api call to get team details

---
+++ score-board/app/src/main/java/com/codecool/scoreboard/teamdetails/TeamPresenter.java
@@ -0,0 +1,64 @@
+package com.codecool.scoreboard.teamdetails;
+
+import com.codecool.scoreboard.ErrorHandlerHelper;
+import com.codecool.scoreboard.apiservice.DataApiService;
+import com.codecool.scoreboard.apiservice.UtilsApi;
+import com.codecool.scoreboard.model.DataAdapter;
+import com.codecool.scoreboard.model.Team;
+import com.codecool.scoreboard.model.TeamResponse;
+
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+
+public class TeamPresenter implements TeamContract.Presenter {
+
+    private TeamContract.View view;
+    private DataApiService dataApiService;
+
+    public TeamPresenter(TeamContract.View view) {
+        this.view = view;
+        dataApiService = UtilsApi.getApiService();
+    }
+
+    @Override
+    public void onAttach() {
+        view.setPresenter(this);
+    }
+
+    @Override
+    public void onDetach() {
+        this.view = null;
+    }
+
+    public void getTeamById(int id) {
+        dataApiService.getTeamById(id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<TeamResponse>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(TeamResponse response) {
+                        TeamResponse.Teams teamResponse = response.getTeams().get(0);
+                        Team team = DataAdapter.convertToTeam(teamResponse);
+                        view.showTeam(team);
+                    }
+
+                    @Override
+                    public void onError(Throwable error) {
+                        error.printStackTrace();
+                        String errorMessage = ErrorHandlerHelper.getErrorMessage(error);
+                        view.showErrorMessage(errorMessage);
+                    }
+                });
+    }
+
+
+}
